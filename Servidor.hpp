d970f0229f1444af51f2c2a31128c16bead03ee1
Héctor Ramón Jiménez <hector0193@gmail.com>
2012-04-14 00:42:28 +0200

    Algoritmo de selección de ruta por niveles implementado. Añadidas algunos métodos a la especificación de la clase Servidor.

--- Servidor.hpp
+++ Servidor.hpp
@@ -7,19 +7,30 @@
 class Servidor
 {
 	public:
-		Servidor(int banda);
+		Servidor(int vel);
 
 		int consultar_peticion() const;
+		int consultar_velocidad() const;
+		int consultar_padre() const;
 		bool esta_libre() const;
+		bool tiene_pelicula() const;
+		bool tiene_padre() const;
+		bool tiene_hijos() const;
 
 		void asignar_peticion(const Peticion &pet);
 		void anadir_pelicula(const Pelicula &peli);
 		void eliminar_pelicula(const Pelicula &peli);
+		void hijos(int &h1, int &h2);
 
 	private:
-		int ancho_banda;
+		int velocidad;
 		int peticion;
 		vector<int> peliculas;
+		int padre;
+		struct Hijos hijos {
+			int h1;
+			int h2;
+		};
 };
 
 #endif
